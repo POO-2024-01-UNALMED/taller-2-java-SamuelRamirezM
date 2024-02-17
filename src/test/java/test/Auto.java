87a31d5bc321efb30ad44f72c0e48ebd5a81b477
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-02-17 10:13:03 -0500

    Creacion de clases

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Auto {
+
+	public static void main(String[] args) {
+		String modelo;
+		int precio;
+		Asiento asientos[];
+		String marca;
+		Motor motor;
+		int registro;
+		public static int cantidadCreados = 0;
+		
+		static int cantidadAsientos() {
+			
+		}
+		
+		static String verificarIntegridad() {
+			
+		}
+	}
+
+}
