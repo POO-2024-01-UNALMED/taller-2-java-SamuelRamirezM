e5cdbd325bdad47a036f146a9c010cdb737a79a5
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-02-19 18:32:18 -0500

    Modificamos métodos de la clase Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,17 +1,35 @@
 package test;
 
 public class Auto {
-
-	public static void main(String[] args) {
-		String modelo;
-		int precio;
-		Asiento asientos[];
-		String marca;
-		Motor motor;
-		int registro;
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
 		
-		
-		
+	int cantidadAsientos() {
+		int cantAsientos = 0;
+		for(int i = 0; i < asientos.length ; i++) {
+			if(asientos[i] != null) {
+				cantAsientos++;
+			}
+		}
+		return cantAsientos;
 	}
-
+		
+	String verificarIntegridad() {
+		if(this.registro == this.motor.registro) {
+			for(int i = 0 ; i < asientos.length ; i++) {
+				if(asientos[i] != null && asientos[i].registro != registro) {
+					return "Las piezas no son originales";
+				}
+			return "Auto original";
+			}
+		} else {
+			return "Las piezas no son originales";
+		}
+	}
+		
 }
